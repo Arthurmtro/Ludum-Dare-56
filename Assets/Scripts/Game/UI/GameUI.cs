9be6787e1019c01bb69fa76099109cb7f370af63
Jose Camara <josec89@gmail.com>
2024-10-06 18:09:23 +0200

    Game UI -> kills and combo

--- Assets/Scripts/Game/UI/GameUI.cs
+++ Assets/Scripts/Game/UI/GameUI.cs
@@ -1,11 +1,30 @@
 
 using Germinator;
+using TMPro;
 using UnityEngine;
 
 public class GameUI : MonoBehaviour
 {
     [SerializeField] private RectTransform hpBar;
+    [SerializeField] private TMP_Text textKills;
+    [SerializeField] private TMP_Text textScore;
+    [SerializeField] private TMP_Text textCombo;
+    [SerializeField] private string[] comboLevelTexts;
     private Animator animator;
+
+    private string ComboLevelText(int level)
+    {
+        if (comboLevelTexts.Length == 0 || level < 0)
+        {
+            return "GOGOGO!";
+        }
+        if (level >= comboLevelTexts.Length)
+        {
+            return comboLevelTexts[comboLevelTexts.Length - 1];
+        }
+
+        return comboLevelTexts[level];
+    }
 
     void Start()
     {
@@ -18,5 +37,17 @@ public class GameUI : MonoBehaviour
 
         hpBar.offsetMax = new Vector2(-hpPerc * 350f, 0f);
         animator.SetTrigger("Hit");
+    }
+
+    public void UpdateScore(int kills, int score)
+    {
+        textKills.text = kills.ToString();
+        textScore.text = score.ToString();
+    }
+
+    public void UpdateCombo(int level)
+    {
+        textCombo.text = ComboLevelText(level);
+        animator.SetInteger("ComboLevel", level);
     }
 }
